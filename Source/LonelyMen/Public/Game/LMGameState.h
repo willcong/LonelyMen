fc3b244b70626d7aa55eb7aa0f127119f364ab4b
willcong <cong1991007@qq.com>
2015-07-11 09:05:22 +0800

    add LonelyMen sln folder & Basic C++ file

---
+++ Source/LonelyMen/Public/Game/LMGameState.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameState.h"
+#include "LMGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LONELYMEN_API ALMGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

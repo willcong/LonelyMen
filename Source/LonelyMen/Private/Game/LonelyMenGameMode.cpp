acc74af64b8e10abbca52e7daad8005ad2c2f963
willcong <cong1991007@qq.com>
2015-07-24 07:37:34 +0800

    修改Mannequin角色动作

--- Source/LonelyMen/Private/Game/LonelyMenGameMode.cpp
+++ Source/LonelyMen/Private/Game/LonelyMenGameMode.cpp
@@ -7,7 +7,7 @@
 ALonelyMenGameMode::ALonelyMenGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/LonelyMen/Blueprints/Player/LMCharacter_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
